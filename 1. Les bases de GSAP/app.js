facbcae5f6b8c2502a2788e1253e1015de72c53e
codwerk <louisws@outlook.fr>
2021-09-07 17:23:38 +0200

    La méthode from()
    Voir le bug  sur la méthode from avec event qui ce répète.

--- 1. Les bases de GSAP/app.js
+++ 1. Les bases de GSAP/app.js
@@ -1,12 +1,22 @@
-// methode .to() -> aller de A a B
+// methode .to() -> aller de A à B (état finale)
 const img1 = document.querySelector('.container-images img:nth-child(1)');
+const bntCat = document.querySelector('.cat-fade');
 
-console.log(img1, gsap);
 
 gsap.to( img1, {
   x: 100, y:100, scale:2, duration: 2
-})
+});
 
 gsap.to( '.title', {
   color: 'red',duration: 1  
+});
+
+
+// methode .from() -> le point A avant l'état finale (B) mais bug sur cet methode
+
+bntCat.addEventListener('click', ()=> {
+  gsap.from( img1, {
+    opacity: 0, duration: 3
+  });
+  
 })
