daa189178b81675724520c8b6438d09db65fb3a6
codwerk <louisws@outlook.fr>
2021-09-07 19:42:40 +0200

    Les "easings"

--- 1. Les bases de GSAP/app.js
+++ 1. Les bases de GSAP/app.js
@@ -1,7 +1,7 @@
 // methode .to() -> aller de A à B (état finale)
 const img1 = document.querySelector('.container-images img:nth-child(1)');
 const bntCat = document.querySelector('.cat-fade');
-
+const bntEasings = document.querySelector('.btn-easings');
 
 /*gsap.to( img1, {
   x: 100, y:100, scale:2, duration: 2
@@ -21,5 +21,14 @@ bntCat.addEventListener('click', ()=> {
 
   // Pour résoudre le bug
   //gsap.fromTo( img1, {opacity: 0}, {opacity: 1, duration: 3});
+  
+  //delay, repeat, repeatDelay
   gsap.to( img1, {x: 100, y:100, scale:2, duration: 2, delay: 2, repeat:1, repeatDelay: 2});
-})
+});
+
+
+
+
+bntEasings.addEventListener('click', ()=> {
+  gsap.to('.container-images img', {y: 100, scale: 1.2, duration: 2, ease:"back.out(4)"});
+});
