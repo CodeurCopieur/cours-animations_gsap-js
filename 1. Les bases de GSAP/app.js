af6fa312d15e040949cfcb543a5be36d3ce8ad0f
codwerk <louisws@outlook.fr>
2021-09-09 11:21:29 +0200

    La fonction "random"

--- 1. Les bases de GSAP/app.js
+++ 1. Les bases de GSAP/app.js
@@ -23,25 +23,28 @@ bntCat.addEventListener('click', ()=> {
   //gsap.fromTo( img1, {opacity: 0}, {opacity: 1, duration: 3});
   
   //delay, repeat, repeatDelay
-  gsap.to( img1, {x: 100, y:100, scale:2, duration: 2, delay: 2, repeat:1, repeatDelay: 2});
+  gsap.to( 'h1', {x: 100, y:100, scale:1.2, duration: 2, delay: 2, repeat:1, repeatDelay: 2});
 });
 
 
 // easings et stagger
 
-/*
+
   bntEasings.addEventListener('click', ()=> {
     gsap.to('.container-images img', {y: 100, scale: 1.2, duration: 2, ease:"back.out(4)", stagger:
-        { amount: 1 OU
+        { //amount: 1 OU
           each: .5,
-        from: "center" }  OU
+        from: "center" }  /*OU
         index => {
           return index * .5
-        }
+        }*/
     });
 
-   stagger: 0.4
+  // OU stagger: 0.4
   });
-*/
 
-gsap.to('.container-btns button', {duration: .2, x: 50, stagger: .2});
+gsap.to('.container-images img', {duration: .2, x: 50, stagger: .2});
+
+gsap.to( '.btn', {
+  duration: .2, x: "random(-100,100)", ease:"power4", stagger: .2
+});
