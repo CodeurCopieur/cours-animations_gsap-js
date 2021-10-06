4bf69cba37fc4cb8b88ba53ed80a6d65c5ffc97c
codwerk <louisws@outlook.fr>
2021-10-06 21:03:35 +0200

    La position du Trigger et du Scroll

--- 2. Animer au scroll/app.js
+++ 2. Animer au scroll/app.js
@@ -5,6 +5,9 @@ gsap.to('.b2 h2', {
   autoAlpha: 1,
   scrollTrigger: {
     trigger: ".b2 h2",
-    markers: true
+    markers: true,
+    //    Trigger Scroller
+    start: "top center", // 50% ou center-=200
+    end: "center 20%"//`+=${document.querySelector(".b2 h2").offsetHeight}`
   }
 })
