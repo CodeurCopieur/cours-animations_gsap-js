5bb9da813ea1535abe31abfc242260f77a6eac01
codwerk <louisws@outlook.fr>
2021-10-06 22:36:44 +0200

    Les évenements et actions

--- 2. Animer au scroll/app.js
+++ 2. Animer au scroll/app.js
@@ -7,7 +7,14 @@ gsap.to('.b2 h2', {
     trigger: ".b2 h2",
     markers: true,
     //    Trigger Scroller
-    start: "top center", // 50% ou center-=200
-    end: "center 20%"//`+=${document.querySelector(".b2 h2").offsetHeight}`
+    start: "top center+=200", // 50% ou center-=200
+    end: "center 20%",//`+=${document.querySelector(".b2 h2").offsetHeight}`
+    // onEnter onLeave onEnterBack onLeaveBack
+    toggleActions: "play pause reverse reset",
+    // play pause resume reset restart complete reverse none
+    onEnter: () => console.log('Enter'),
+    onLeave: () => console.log('Leave'),
+    onEnterBack: () => console.log('EnterBack'),
+    onLeaveBack: () => console.log('LeaveBack'),
   }
 })
