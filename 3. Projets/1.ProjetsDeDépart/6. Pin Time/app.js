72e845eff4a02f6f5a43bfafa8bcb77fb23986a6
codwerk <louisws@outlook.fr>
2021-10-15 22:37:47 +0200

    end

--- 3. Projets/1.ProjetsDeDépart/6. Pin Time/app.js
+++ 3. Projets/1.ProjetsDeDépart/6. Pin Time/app.js
@@ -10,5 +10,39 @@ const src = [
     "476 | 1492",
     "1492 | 1792",
     "1792 | AJD",
-  ]
+  ];
+
+  ScrollTrigger.create({
+    trigger: '.sections',
+    pin: '.tooltip',
+    //markers: true,
+    start: 'top 40%',
+    end: 'bottom 55%'
+  })
   
+const sections = gsap.utils.toArray('.section');
+
+sections.forEach((section, i) => {
+  ScrollTrigger.create({
+    trigger: section,
+    start: 'top center',
+    end: 'bottom center',
+    markers: true,
+    onEnter: () => {
+      gsap.set('.tooltip-img', {
+        attr: {src: src[i]}
+      })
+      gsap.set('.tooltip p', {
+        innerText: dates[i]
+      })
+    },
+    onEnterBack: () => {
+      gsap.set('.tooltip-img', {
+        attr: {src: src[i]}
+      })
+      gsap.set('.tooltip p', {
+        innerText: dates[i]
+      })
+    }
+  })
+});
