77881b238ec61b3d9fc5ed978fb30e49da5377c2
codwerk <louisws@outlook.fr>
2021-10-22 23:17:47 +0200

    Navigation Links

---
+++ GreenSock - Bella/gsap3-bella-starting-files/00-Full-Page/js/main.js
@@ -0,0 +1,32 @@
+gsap.registerPlugin(ScrollTrigger);
+
+function initNavigation() {
+
+    const mainNavLinks = gsap.utils.toArray('.main-nav a');
+
+    console.log(mainNavLinks);
+
+    mainNavLinks.forEach( link => {
+        link.addEventListener('mouseleave', ({target}) => {
+            console.log(target);
+            // add class
+            target.classList.add('animate-out');
+
+            setTimeout(() => {
+                // remove class
+                target.classList.remove('animate-out');
+            }, 300)
+        })
+    });
+
+}
+
+function init(){
+    
+    initNavigation();
+
+}
+
+window.addEventListener('load', function(){
+    init();
+});
