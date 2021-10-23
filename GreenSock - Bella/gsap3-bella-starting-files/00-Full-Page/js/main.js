ca172d0671f86743ff3965a8e43a6b39b13c433d
codwerk <louisws@outlook.fr>
2021-10-23 13:45:16 +0200

    Navigation Away

--- GreenSock - Bella/gsap3-bella-starting-files/00-Full-Page/js/main.js
+++ GreenSock - Bella/gsap3-bella-starting-files/00-Full-Page/js/main.js
@@ -5,6 +5,7 @@ function initNavigation() {
     // Navigation Links
 
     const mainNavLinks = gsap.utils.toArray('.main-nav a');
+    const mainNavLinksRev = gsap.utils.toArray('.main-nav a').reverse();
 
     mainNavLinks.forEach( link => {
         link.addEventListener('mouseleave', ({target}) => {
@@ -21,13 +22,28 @@ function initNavigation() {
 
     // Navigation Toggle Class
 
+    function navAnimation(direction) {
+        console.log(direction);
+        const scrolling = direction === 1;
+        const links = scrolling ? mainNavLinks : mainNavLinksRev
+        return gsap.to(links, {
+            duration: .3, stagger: .05,
+            autoAlpha: () => scrolling ? 0 : 1,
+            y: () => scrolling ? 20 : 0,
+            ease: 'Power4.out'
+        })
+    }
+
     ScrollTrigger.create({
         markers: true,
         start: 100,
+        end: 'bottom bottom-=200',
         toggleClass: {
             targets: 'body',
             className: 'has-scrolled'
-        }
+        },
+        onEnter: ({direction}) => navAnimation(direction),
+        onLeaveBack: ({direction}) => navAnimation(direction)
     })
 }
 
