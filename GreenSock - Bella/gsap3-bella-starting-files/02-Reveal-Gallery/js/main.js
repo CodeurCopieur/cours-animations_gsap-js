f9e7681f4165898fa7727bf6af47d8547c547753
codwerk <louisws@outlook.fr>
2021-10-25 23:39:55 +0200

    reveal text

--- GreenSock - Bella/gsap3-bella-starting-files/02-Reveal-Gallery/js/main.js
+++ GreenSock - Bella/gsap3-bella-starting-files/02-Reveal-Gallery/js/main.js
@@ -7,13 +7,17 @@ function initHoverReveal() {
         
         // obtenir des composants pour l'animation
         section.imageBlock = section.querySelector('.rg__image');
+        section.image = section.querySelector('.rg__image img');
         section.mask = section.querySelector('.rg__image--mask');
         section.text = section.querySelector('.rg__text');
         section.textHeight = section.querySelector('.rg__text--copy');
+        section.textMask = section.querySelector('.rg__text--mask');
+        section.textP = section.querySelector('.rg__text--copy p');
 
         // réinitialiser la position initiale
-        gsap.set(section.imageBlock, {yPercent: -101});
-        gsap.set(section.mask, {yPercent: 100});
+        gsap.set([section.imageBlock, section.textMask], {yPercent: -101});
+        gsap.set([section.mask, section.textP], {yPercent: 100});
+        gsap.set(section.image, {scale: 1.2});
 
         // ajouter des écouteurs d'événement à chaque section
         section.addEventListener('mouseenter', createHoverReveal)
@@ -27,7 +31,7 @@ function getTextHeight(textHeight) {
 
 function createHoverReveal({type, target}){
     
-    const {imageBlock, mask, text, textHeight} = target;
+    const {imageBlock, mask, text, textHeight, textMask, textP, image} = target;
 
     let tl = gsap.timeline({
         defaults: {
@@ -39,15 +43,17 @@ function createHoverReveal({type, target}){
     if(type === 'mouseenter') {
 
         tl
-        .to([mask, imageBlock], {yPercent: 0})
-        .to(text, {y: () => -getTextHeight(textHeight)/2})
+        .to([mask, imageBlock, textMask, textP], {yPercent: 0})
+        .to(text, {y: () => -getTextHeight(textHeight)/2}, 0)
+        .to(image, {duration: 1.1, scale: 1}, 0)
 
     }else if(type === 'mouseleave') {
 
         tl
-        .to(imageBlock, {yPercent: -101})
-        .to(mask, {yPercent: 100}, 0)
-        .to(text, {y: 0})
+        .to([mask, textP], {yPercent: 100})
+        .to([imageBlock, textMask], {yPercent: -101}, 0)
+        .to(text, {y: 0}, 0)
+        .to(image, {scale: 1}, 0)
     }
 
     return tl;
