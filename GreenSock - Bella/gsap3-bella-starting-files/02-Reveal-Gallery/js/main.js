4b302fd0ac2e806afc6ae63dd6375214c9ddf890
codwerk <louisws@outlook.fr>
2021-10-25 19:55:42 +0200

    reset positions

--- GreenSock - Bella/gsap3-bella-starting-files/02-Reveal-Gallery/js/main.js
+++ GreenSock - Bella/gsap3-bella-starting-files/02-Reveal-Gallery/js/main.js
@@ -1,8 +1,31 @@
 gsap.registerPlugin(ScrollTrigger);
 
+function initHoverReveal() {
+
+    const sections = document.querySelectorAll('.rg__column');
+    sections.forEach(section => {
+        
+        // obtenir des composants pour l'animation
+        const imageBlock = section.querySelector('.rg__image');
+        const mask = section.querySelector('.rg__image--mask');
+
+        // réinitialiser la position initiale
+        gsap.set(imageBlock, {yPercent: -101});
+        gsap.set(mask, {yPercent: 100});
+
+        // ajouter des écouteurs d'événement à chaque section
+        section.addEventListener('mouseenter', createHoverReveal)
+        section.addEventListener('mouseleave', createHoverReveal)
+    });
+}
+
+function createHoverReveal(e){
+    console.log(e.type);
+}
+
 function init(){
     
-    // start here
+    initHoverReveal();
 
 }
 
