5e227b82f3acf4e26e1dfa9169ac123675d21cb4
codwerk <louisws@outlook.fr>
2021-11-15 18:30:39 +0100

    Loading Animation In

--- GreenSock - Bella/gsap3-bella-starting-files/05-Intro-Animation/js/main.js
+++ GreenSock - Bella/gsap3-bella-starting-files/05-Intro-Animation/js/main.js
@@ -1,8 +1,39 @@
 gsap.registerPlugin(ScrollTrigger);
 
+const select = e => document.querySelector(e);
+const selectAll = e => document.querySelectorAll(e);
+
+function initLoader() {
+
+    const tlLoaderIn = gsap.timeline({
+        id: 'tlLoaderIn',
+        defaults: {
+            duration: 1.1,
+            ease: 'Power2.out'
+        }
+    });
+
+    const loaderInner = select('.loader .inner');
+    const image = select('.loader__image img');
+    const mask = select('.loader__image--mask');
+    const line1 = select('.loader__image--mask:nth-child(1) span');
+    const line2 = select('.loader__image--mask:nth-child(2) span');
+
+    tlLoaderIn
+        .from(loaderInner, {
+            scaleY: 0,
+            transformOrigin: 'bottom'
+        }, .2)
+        .addLabel('revealImage')
+        .from(mask, {yPercent: 100}, 'revealImage-=.6')
+        .from(image, {yPercent: -50}, 'revealImage-=.6')
+
+    GSDevTools.create({paused:true})
+}
+
 function init(){
     
-    // start here
+    initLoader();
 
 }
 
