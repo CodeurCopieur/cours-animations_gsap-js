ee7aa751dd49b42a69be8da8717a7863560d591d
codwerk <louisws@outlook.fr>
2021-11-22 22:47:57 +0100

    Loading Animation Out

--- GreenSock - Bella/gsap3-bella-starting-files/05-Intro-Animation/js/main.js
+++ GreenSock - Bella/gsap3-bella-starting-files/05-Intro-Animation/js/main.js
@@ -9,15 +9,19 @@ function initLoader() {
         id: 'tlLoaderIn',
         defaults: {
             duration: 1.1,
-            ease: 'Power2.out'
-        }
+            ease: 'power2.out'
+        },
+        onComplete: () => select('body').classList.remove('is-loading')
     });
 
     const loaderInner = select('.loader .inner');
     const image = select('.loader__image img');
     const mask = select('.loader__image--mask');
-    const line1 = select('.loader__image--mask:nth-child(1) span');
-    const line2 = select('.loader__image--mask:nth-child(2) span');
+    const line1 = select('.loader__title--mask:nth-child(1) span');
+    const line2 = select('.loader__title--mask:nth-child(2) span');
+    const lines = selectAll('.loader__title--mask');
+    const loader = select('.loader');
+    const loaderContent = select('.loader__content');
 
     tlLoaderIn
         .from(loaderInner, {
@@ -26,9 +30,30 @@ function initLoader() {
         }, .2)
         .addLabel('revealImage')
         .from(mask, {yPercent: 100}, 'revealImage-=.6')
-        .from(image, {yPercent: -50}, 'revealImage-=.6')
+        .from(image, {yPercent: -80}, 'revealImage-=.6')
+        .from([line1, line2], {yPercent: 100, stagger: .1}, 'revealImage-=.4');
+    
+    const tlLoaderOut = gsap.timeline({
+        id: 'tlLoaderOut',
+        defaults: {
+            duration: 1.2,
+            ease: 'power2.inOut'
+        },
+        delay: 1
+    });
 
-    GSDevTools.create({paused:true})
+    tlLoaderOut
+        .to(lines, {yPercent: -500, stagger: .2}, 0)
+        .to([loader, loaderContent], {yPercent: -100}, .2)
+        .from('#main', {y: 150}, .2)
+
+
+    const tlLoader = gsap.timeline();
+    tlLoader
+        .add(tlLoaderIn)
+        .add(tlLoaderOut)
+
+    //GSDevTools.create({paused:true})
 }
 
 function init(){
