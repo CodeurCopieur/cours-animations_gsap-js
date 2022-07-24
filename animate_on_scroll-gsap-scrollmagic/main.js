c5dcb98018f26f414d4438a57ed5c841761666da
Widdy LOUIS <louisws@outlook.fr>
2022-07-24 22:06:33 +0200

    Revert "Revert "first commit""
    This reverts commit 365c2158ae61260507e85e2b17f352af6340b86b.

---
+++ animate_on_scroll-gsap-scrollmagic/main.js
@@ -0,0 +1,10 @@
+import gsap from 'gsap'
+import ScrollMagic from 'scrollmagic'
+
+
+
+document.addEventListener('DOMContentLoaded', ()=> {
+  const tl = gsap.timeline();
+  // init controller
+  var controller = new ScrollMagic.Controller();
+});
