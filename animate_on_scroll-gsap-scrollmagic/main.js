135dcf1291cd0e164484d4a44b86d5f44a56b80e
Widdy LOUIS <louisws@outlook.fr>
2022-07-25 22:48:58 +0200

    create third scene

--- animate_on_scroll-gsap-scrollmagic/main.js
+++ animate_on_scroll-gsap-scrollmagic/main.js
@@ -78,4 +78,44 @@ document.addEventListener('DOMContentLoaded', () => {
     .setTween(tl2)
     .setPin('.second-section')
     .addTo(controller);
+  
+  let tl3 = new TimelineMax();
+  tl3
+  .to('.section_3_01', 4, {
+    y: -250,
+    ease: Power3.easeInOut
+  })
+  .to('.section_3_02', 4, {
+    y: -200,
+    ease: Power3.easeInOut
+  }, '-=4')
+  .to('.section_3_03', 4, {
+    y: -100,
+    ease: Power3.easeInOut
+  }, '-=4')
+  .to('.section_3_04', 4, {
+    y: 0,
+    ease: Power3.easeInOut
+  }, '-=4')
+  .to('.section_3_05', 4, {
+    y: 100,
+    ease: Power3.easeInOut
+  }, '-=4')
+  .to('.section_3_06', 4, {
+    y: 200,
+    ease: Power3.easeInOut
+  }, '-=4')
+
+
+  let scene3 = new ScrollMagic.Scene({
+      triggerElement: '.third-section',
+      duration: '100%',
+      triggerHook: 0,
+      offset: '200'
+    })
+    .setTween(tl3)
+    .setPin('.third-section')
+    .addTo(controller);
+
+
 });
