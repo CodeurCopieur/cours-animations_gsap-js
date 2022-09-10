0525345e5133a81d273db808a8e4718a07538d15
Widdy LOUIS <louisws@outlook.fr>
2022-09-10 11:22:25 +0200

    end commit

--- scroll_trigger-gsap/index.js
+++ scroll_trigger-gsap/index.js
@@ -1,5 +1,30 @@
 function initScroll() {
+  // register scrolltrigger plugin
+  gsap.registerPlugin(ScrollTrigger);
 
+  // ScrollTrigger MatchMedia -> responsive animation.
+  // animé uniquement sur les appareils de bureau et supérieurs
+
+  ScrollTrigger.matchMedia({
+    "(min-width: 992px)": function() {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".product_image",
+          markers: true,
+          start: "top+=10 10%",
+          end: "bottom-=10 10%",
+          scrub: true,
+          pin: true
+        }
+      });
+      tl.to('.step_one_content', 
+        {autoAlpha: 0, duration: .5})
+        .to('.product_image', 
+        {x: '-100%', duration: 1}) // déplacer l'image vers la gauche
+        .fromTo('.step_two_content', 
+        {autoAlpha: 0}, {autoAlpha: 1}) // monter la deuxième blocs html
+    }
+  })
 }
 
 function init(){
