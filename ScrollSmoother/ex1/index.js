eb95424f658ce5013e123b808eddc4d474197572
Widdy LOUIS <louisws@outlook.fr>
2023-01-18 02:00:51 +0100

    Update index.js

--- ScrollSmoother/ex1/index.js
+++ ScrollSmoother/ex1/index.js
@@ -5,7 +5,21 @@ let smoother = ScrollSmoother.create({
   wrapper: "#smooth-wrapper",
   content: "#smooth-content",
   smooth: 2,
-  smoothTouch: .1, // défillement appraeils tactiles
+  smoothTouch: .1, // défillement appareils tactiles
+  effects: true // ajouter des effets lisses
 });
 
 // Faites défiler facilement jusqu'à un certain point
+
+let btn  = document.querySelector('.button')
+
+btn.addEventListener('click', e => {
+  // smoother.scrollTo('.box-f', true, 'center center')
+  // pixel a la place de '.box-f'
+
+  // gsap.to(smoother, {
+  //   scrollTop:  smoother.offset(".box-f", true, "center center"),
+  //   duration: 2,
+  //   ease: 'back.out'
+  // })
+})
